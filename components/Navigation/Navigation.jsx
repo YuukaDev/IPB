2ac6c2979b204b0b228539f20ee80fcd29564792
YuukaDev <vule.amv1@gmail.com>
2022-05-02 23:49:02 +0200

    Update Navigation.jsx

--- components/Navigation/Navigation.jsx
+++ components/Navigation/Navigation.jsx
@@ -1,423 +1,62 @@
-import NextLink from "next/link";
-import NextImage from "next/image";
-import {
-  Box,
-  Flex,
-  Link,
-  Icon,
-  Badge,
-  useColorMode,
-  Button,
-} from "@chakra-ui/react";
-
-import ipbLogo from "../../public/logo_1.png";
-
+import Image from "next/image";
+import Link from "next/link";
+import logoImage from "../../images/logo_1.png";
 import { AiOutlineSearch, AiOutlineShoppingCart } from "react-icons/ai";
-import { useCartState } from "../../utils/StoreContext";
-import DarkMode from "../ThemeChanger/DarkMode";
 
 export default function Navigation() {
-  const { line_items } = useCartState();
-  //const { colorMode } = useColorMode();
   return (
-    <Box
-      zIndex="1"
-      boxShadow="dark-lg"
-      as="nav"
-      top="0"
-      pos="sticky"
-      fontSize="1em"
-      color="#fff"
-      fontWeight="500"
-      backgroundColor="#101010"
-      display="flex"
-      width="100%"
-      justifyContent="space-between"
-      alignItems="center"
-      padding="13px 10%"
-    >
-      <NextLink href="/">
-        <NextImage
-          style={{
-            cursor: "pointer",
-          }}
-          width="80"
-          height="55"
-          src={ipbLogo}
-        />
-      </NextLink>
-      <Flex gap="30px" color="rgb(255, 255, 255)">
-        <NextLink href="#" passHref>
-          <Link
-            transition="0.3s all ease"
-            opacity="1"
-            _hover={{
-              opacity: "0.5",
-              textDecoration: "none",
-              transition: "0.3s all ease",
-            }}
-          >
-            Home
+    <nav>
+      <div className="sticky top-0 shadow-lg flex justify-between px-52 p-3 items-center">
+        <div>
+          <a aria-label="Home" href="/">
+            <Image src={logoImage} alt="logo" width="90" height="50" />
+          </a>
+        </div>
+        <div className="flex flex-wrap gap-5 text-navigationColor">
+          <Link href="/" passHref>
+            <a aria-label="Home">
+              <p className=" ">Home</p>
+            </a>
           </Link>
-        </NextLink>
-        <NextLink href="#" passHref>
-          <Link
-            transition="0.3s all ease"
-            opacity="1"
-            _hover={{
-              opacity: "0.5",
-              textDecoration: "none",
-              transition: "0.3s all ease",
-            }}
-          >
-            Shop All
+          <Link href="/" passHref>
+            <a aria-label="Home">
+              <p className=" ">Shop All</p>
+            </a>
           </Link>
-        </NextLink>
-        <NextLink href="#" passHref>
-          <Link
-            transition="0.3s all ease"
-            opacity="1"
-            _hover={{
-              opacity: "0.5",
-              textDecoration: "none",
-              transition: "0.3s all ease",
-            }}
-          >
-            Hottest
+          <Link href="/" passHref>
+            <a aria-label="Home">
+              <p className=" ">Hottest</p>
+            </a>
           </Link>
-        </NextLink>
-        <NextLink href="#" passHref>
-          <Link
-            transition="0.3s all ease"
-            opacity="1"
-            _hover={{
-              opacity: "0.5",
-              textDecoration: "none",
-              transition: "0.3s all ease",
-            }}
-          >
-            About
+          <Link href="/" passHref>
+            <a aria-label="Home">
+              <p className=" ">Newest</p>
+            </a>
           </Link>
-        </NextLink>
-        <NextLink href="#" passHref>
-          <Link
-            transition="0.3s all ease"
-            opacity="1"
-            _hover={{
-              opacity: "0.5",
-              textDecoration: "none",
-              transition: "0.3s all ease",
-            }}
-          >
-            Support
+          <Link href="/" passHref>
+            <a aria-label="Home">
+              <p className=" ">About Us</p>
+            </a>
           </Link>
-        </NextLink>
-      </Flex>
-      <Flex width="0" gap="15px" fontSize="24px" cursor="pointer">
-        <Icon
-          transition="0.3s all ease"
-          opacity="1"
-          _hover={{
-            opacity: "0.5",
-            textDecoration: "none",
-            transition: "0.3s all ease",
-          }}
-          as={AiOutlineSearch}
-        />
-        <Link href="/cart">
-          <Icon
-            transition="0.3s all ease"
-            opacity="1"
-            _hover={{
-              opacity: "0.5",
-              textDecoration: "none",
-              transition: "0.3s all ease",
-            }}
-            as={AiOutlineShoppingCart}
-          />
-        </Link>
-        {JSON.stringify(line_items.length > 0) ? (
-          <Badge
-            ml="-20px"
-            display="flex"
-            w="12px"
-            h="12px"
-            borderRadius="50%"
-            justifyContent="center"
-            alignItems="center"
-            colorScheme="purple"
-            cursor="auto"
-          >
-            {JSON.stringify(line_items.length)}
-          </Badge>
-        ) : (
-          ""
-        )}
-        <DarkMode />
-      </Flex>
-    </Box>
+          <Link href="/" passHref>
+            <a aria-label="Home">
+              <p className=" ">Contact</p>
+            </a>
+          </Link>
+        </div>
+        <div className="flex gap-3">
+          <Link href="/" passHref>
+            <a>
+              <AiOutlineSearch fontSize="1.2em" />
+            </a>
+          </Link>
+          <Link href="/" passHref>
+            <a>
+              <AiOutlineShoppingCart fontSize="1.2em" />
+            </a>
+          </Link>
+        </div>
+      </div>
+    </nav>
   );
 }
-
-/*
-    <Box
-        zIndex="1"
-        as="nav"
-        top="0"
-        pos="sticky"
-        letterSpacing=".12em"
-        fontSize="14px"
-        color="#1b133c"
-        textTransform="uppercase"
-        fontWeight="500"
-        backgroundColor="#ffffff"
-        display="flex"
-        alignItems="center"
-        padding="30px 10%"
-      >
-        <NextLink href="/">
-          <Image
-            cursor="pointer"
-            src={
-              "https://cdn.shopify.com/s/files/1/0070/1700/5113/files/AtsukoSiteLogo_9f065aae-ad08-42df-9b9d-da9836dde88a_120x.png?v=1613893342"
-            }
-          />
-        </NextLink>
-        <Flex
-          width="85%"
-          justifyContent="center"
-          alignItems="center"
-          gap="20px"
-        >
-          <NextLink href="#" passHref>
-            <Link
-              _hover={{
-                textDecoration: "none",
-              }}
-            >
-              Home
-            </Link>
-          </NextLink>
-          <NextLink href="#" passHref>
-            <Link
-              _hover={{
-                textDecoration: "none",
-              }}
-            >
-              Shop All
-            </Link>
-          </NextLink>
-          <NextLink href="#" passHref>
-            <Link
-              _hover={{
-                textDecoration: "none",
-              }}
-            >
-              Clothing
-            </Link>
-          </NextLink>
-          <NextLink href="#" passHref>
-            <Link
-              _hover={{
-                textDecoration: "none",
-              }}
-            >
-              Accessories
-            </Link>
-          </NextLink>
-          <NextLink href="#" passHref>
-            <Link
-              _hover={{
-                textDecoration: "none",
-              }}
-              bg="#f7453f"
-              p="6px"
-              borderRadius="md"
-              color="#fff"
-            >
-              Blog
-            </Link>
-          </NextLink>
-        </Flex>
-        <Flex width="0" gap="15px" fontSize="24px" cursor="pointer">
-          <Icon as={AiOutlineSearch} />
-          <Icon as={AiOutlineUser} />
-          <NextLink href="/cart" passHref>
-            <Link>
-              <Icon as={AiOutlineShoppingCart} />
-            </Link>
-          </NextLink>
-          {JSON.stringify(products.length > 0) ? (
-            <Badge
-              ml="-20px"
-              display="flex"
-              w="12px"
-              h="12px"
-              borderRadius="50%"
-              justifyContent="center"
-              alignItems="center"
-              colorScheme="purple"
-              cursor="auto"
-            >
-              {JSON.stringify(products.length)}
-            </Badge>
-          ) : (
-            ""
-          )}
-        </Flex>
-      </Box>
-*/
-
-/*
- <Box
-      boxShadow="lg"
-      as="nav"
-      display="flex"
-      alignItems="center"
-      bg="rgb(16, 16, 16)"
-      zIndex="1"
-      top="0"
-      pos="sticky"
-      fontSize="14px"
-      color="#000"
-      textTransform="uppercase"
-      fontWeight="500"
-      padding="15px 7%"
-    >
-      <NextLink href="/">
-        <NextImage width="80" height="55" src={ipbLogo} />
-      </NextLink>
-      <Flex
-        color="#fff"
-        width="90%"
-        justifyContent="center"
-        alignItems="center"
-        gap="20px"
-      >
-        <NextLink href="/">
-          <Link
-            fontSize="1.1em"
-            transition="0.5s all ease"
-            textDecoration="none"
-            _hover={{
-              transition: "0.5s all ease",
-              transform: "scale(1.1)",
-              textDecoration: "none",
-            }}
-          >
-            <Button
-              fontSize={{
-                sm: "1em",
-                md: "0.5em",
-                lg: "1em",
-                xl: "1.2em",
-              }}
-              size="md"
-              variant="ghost"
-            >
-              Home
-            </Button>
-          </Link>
-        </NextLink>
-        <NextLink href="/">
-          <Link
-            fontSize="1.1em"
-            transition="0.5s all ease"
-            textDecoration="none"
-            _hover={{
-              transition: "0.5s all ease",
-              transform: "scale(1.1)",
-              textDecoration: "none",
-            }}
-          >
-            <Button
-              fontSize={{
-                sm: "1em",
-                md: "0.5em",
-                lg: "1em",
-                xl: "1.2em",
-              }}
-              size="md"
-              variant="ghost"
-            >
-              Shop
-            </Button>
-          </Link>
-        </NextLink>
-        <NextLink href="/">
-          <Link
-            fontSize="1.1em"
-            transition="0.5s all ease"
-            textDecoration="none"
-            _hover={{
-              transition: "0.5s all ease",
-              transform: "scale(1.1)",
-              textDecoration: "none",
-            }}
-          >
-            <Button
-              fontSize={{
-                sm: "1em",
-                md: "0.5em",
-                lg: "1em",
-                xl: "1.2em",
-              }}
-              size="md"
-              variant="ghost"
-            >
-              About
-            </Button>
-          </Link>
-        </NextLink>
-        <NextLink href="/">
-          <Link
-            fontSize="1.1em"
-            transition="0.5s all ease"
-            textDecoration="none"
-            _hover={{
-              transition: "0.5s all ease",
-              transform: "scale(1.1)",
-              textDecoration: "none",
-            }}
-          >
-            <Button
-              fontSize={{
-                sm: "1em",
-                md: "0.5em",
-                lg: "1em",
-                xl: "1.2em",
-              }}
-              size="md"
-              variant="ghost"
-            >
-              Support
-            </Button>
-          </Link>
-        </NextLink>
-      </Flex>
-      <Flex color="#fff" width="0" gap="15px" fontSize="24px" cursor="pointer">
-        <Icon as={AiOutlineSearch} />
-        <NextLink href="/cart" passHref>
-          <Icon as={AiOutlineShoppingCart} />
-        </NextLink>
-        {line_items.length > 0 ? (
-          <Badge
-            ml="-20px"
-            display="flex"
-            w="12px"
-            h="12px"
-            borderRadius="50%"
-            justifyContent="center"
-            alignItems="center"
-            colorScheme="purple"
-            cursor="auto"
-          >
-            {line_items.length}
-          </Badge>
-        ) : (
-          ""
-        )}
-        <DarkMode />
-      </Flex>
-    </Box>
-*/
