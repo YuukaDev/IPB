8b29d656229d5523abb2320903af68a2fbb918c0
YuukaDev <vule.amv1@gmail.com>
2022-05-14 23:55:12 +0200

    Update Product.jsx

--- components/ProductOverview/Product.jsx
+++ components/ProductOverview/Product.jsx
@@ -1,58 +1,43 @@
-import Link from "next/link";
+import Swiper from "./Swiper";
 
 const ProductBox = ({ product }) => {
   const delivery = product.has.digital_delivary;
   const inStock = product.inventory.available >= 0;
   return (
-    <div>
-      <div className="flex text-navigationColor mt-96 gap-5">
-        <div className="justify-center items-center flex">
-          <img
-            className="w-imageBox h-imageBox rounded-3xl"
-            src={product.image.url}
-          />
+    <div class="bg-productBox max-w-sm rounded overflow-hidden shadow-lg">
+      <img class="w-full" src={product.image.url} alt={product.name} />
+      <div class="px-6 py-4">
+        <div class="font-bold text-xl uppercase tracking-widest text-center mb-2">
+          {product.name}
         </div>
-        <div className="flex rounded-3xl bg-gray-300 justify-center flex-col items-center h-productBox w-productBox">
-          <h1 className="mt-5 text-3xl uppercase tracking-wide">
-            {product.name}
-          </h1>
-          <div className="mt-5 gap-2 justify-center items-center">
-            {delivery ? (
-              <span>✔️ Digital Delivery</span>
-            ) : (
-              <span>❌ Digital Delivery</span>
-            )}
-            {" | "}
-            {inStock ? <span>✔️ In Stock</span> : <span>❌ In Stock</span>}
-          </div>
-          <hr className="mt-5" />
-          <p className="text-2xl tracking-wide mt-5">
-            Price - {product.price.formatted_with_symbol}
-          </p>
-          <button className="font-bold mt-5 bg-productButton rounded-3xl tracking-widest bg-transparent uppercase py-3 px-5 text-heroColor">
+        <div className="text-center">
+          {delivery ? (
+            <span>✔️ Digital Delivery</span>
+          ) : (
+            <span>❌ Digital Delivery</span>
+          )}
+          {" | "}
+          {inStock ? <span>✔️ In Stock</span> : <span>❌ In Stock</span>}
+        </div>
+        <p class="text-gray-700 text-3xl text-center mt-8 mb-8">
+          Price - {product.price.formatted_with_symbol}
+        </p>
+        <div className="flex justify-center items-center">
+          <button className="font-bold bg-productButton rounded-3xl tracking-widest uppercase py-3 px-5 text-heroColor">
             Add To Cart
           </button>
-          <div className="flex gap-3 mt-5 mb-5">
-            <a
-              style={{
-                border: "1px solid black",
-                padding: "5px",
-              }}
-              href="/"
-            >
-              {product.categories[1].name}
-            </a>
-            <a
-              style={{
-                border: "1px solid black",
-                padding: "5px",
-              }}
-              href="/"
-            >
-              {product.categories[0].name}
-            </a>
-          </div>
         </div>
+      </div>
+      <div class="flex justify-center items-center px-6 pt-4 pb-2">
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+          {product.categories[1].name}
+        </span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+          {product.categories[0].name}
+        </span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+          Adventure
+        </span>
       </div>
     </div>
   );
@@ -60,18 +45,18 @@ const ProductBox = ({ product }) => {
 
 export default function Product({ product }) {
   return (
-    <div className="flex justify-center flex-col items-center">
-      <div
-        className="flex flex-col justify-center items-center"
-        style={{
-          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('${product.assets[1].url}')`,
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          height: "50vh",
-          width: "100%",
-        }}
-      >
+    <div
+      style={{
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('${product.assets[1].url}')`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        height: "80vh",
+        width: "100%",
+      }}
+    >
+      <div className="flex justify-center items-center h-screen">
+        <Swiper />
         <ProductBox product={product} />
       </div>
     </div>
