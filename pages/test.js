a77ab543111736b7095d30da000b9ce6aca85198
YuukaDev <vule.amv1@gmail.com>
2022-05-22 18:03:51 +0200

    Update test.js

--- pages/test.js
+++ pages/test.js
@@ -4,7 +4,9 @@ import commerce from "../lib/commerce";
 
 export async function getStaticProps() {
     const { data: category } = await commerce.categories.list();
-    const { data: products } = await commerce.products.list();
+    const { data: products } = await commerce.products.list({
+        category_slug: [category.name]
+    });
 
     return {
         props: {
@@ -15,20 +17,19 @@ export async function getStaticProps() {
 }
 
 export default function App({ products, category }) {
-    const [clicked, setClicked] = useState(false);
-    console.log(category);
+    const [items, setItems] = useState(products);
     return (
         <div>
             <div className="text-navigationColor text-center">
                 <div className="flex justify-center items-center gap-8 mt-10">
                     {category.map((categories) => (
                         <div key={categories.id}>
-                            <button>{categories.name}</button>
+                            <button onClick={(e) => setItems(e.target.value)}>{categories.name}</button>
                         </div>
                     ))}
                 </div>
                 <div className="mt-16 inline-grid md:grid-cols-2 md:gap-8 lg:grid-cols-3 lg:gap-8">
-                    {products.map((product) => (
+                    {items.map((product) => (
                         <Card key={product.id} product={product} />
                     ))}
                 </div>
