5c933a2e5a9f898492cfb872adbe3223f8400529
YuukaDev <vule.amv1@gmail.com>
2022-05-28 16:21:38 +0200

    Update CartItem.jsx

--- components/Cart/CartItem.jsx
+++ components/Cart/CartItem.jsx
@@ -1,9 +1,8 @@
 import commerce from "../../lib/commerce";
-import { useCartDispatch } from "../../utils/StoreContext";
+import useShop, { useCartDispatch } from "../../utils/StoreContext";
 
 export const CartItem = ({ image, id, name, price, quantity, line_total }) => {
   const { setCart } = useCartDispatch();
-
   const handleUpdateCart = ({ cart }) => setCart(cart);
 
   const removeItem = () => commerce.cart.remove(id).then(handleUpdateCart);
@@ -20,16 +19,52 @@ export const CartItem = ({ image, id, name, price, quantity, line_total }) => {
     commerce.cart.update(id, { quantity: quantity + 1 }).then(handleUpdateCart);
 
   return (
-    <div className="flex justify-center items-center gap-10">
-      <img style={{ width: "115px", height: "80px" }} src={image.url} />
-      <p>{name}</p>
-      <p>{price.formatted_with_symbol}</p>
-      <div className="flex gap-3">
-        <button onClick={decrementQuantity}>-</button>
-        <p>{quantity}</p>
-        <button onClick={incrementQuantity}>+</button>
-      </div>
-      <button onClick={removeItem}>&times;</button>
-    </div>
+    <tbody className="shadow-lg">
+      <tr>
+        <td>
+          <img src={image.url} className="max-w-xs rounded" />
+        </td>
+        <td>
+          <div className="flex flex-col text-lg">
+            <p className="text-left text-lg w-72 mb-2">{name}</p>
+            <a
+              onClick={removeItem}
+              className="cursor-pointer text-gray-700 font-bold text-xs text-left"
+            >
+              Remove
+            </a>
+          </div>
+        </td>
+        <td className="text-center">
+          <span className="text-lg font-semibold">
+            {price.formatted_with_symbol}
+          </span>
+        </td>
+        <td className="justify-center items-center">
+          <div className="flex justify-center items-center gap-3">
+            <button className="text-lg" onClick={incrementQuantity}>
+              +
+            </button>
+            <div
+              style={{
+                border: "2px solid #34de01",
+                padding: "5px 12px",
+                borderRadius: "100%",
+              }}
+            >
+              <p className="text-logoGreen">{quantity}</p>
+            </div>
+            <button className="text-lg font-normal" onClick={decrementQuantity}>
+              -
+            </button>
+          </div>
+        </td>
+        <td className="text-center">
+          <span className="text-lg font-semibold">
+            {line_total.formatted_with_symbol}
+          </span>
+        </td>
+      </tr>
+    </tbody>
   );
 };
