c9b9ec3cd872a315291056d563a3937f7f5695ac
YuukaDev <vule.amv1@gmail.com>
2022-06-06 22:17:34 +0200

    Update PaypalCheckoutButton.jsx

--- components/Checkout/PaypalCheckoutButton.jsx
+++ components/Checkout/PaypalCheckoutButton.jsx
@@ -1,30 +1,65 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
+import commerce from "../../lib/commerce";
+import useShop from "../../utils/StoreContext";
 
-export default function PaypalCheckoutButton({ product, submit }) {
+export default function PaypalCheckoutButton({ product }) {
+  const { cart } = useShop();
+  const [token, setToken] = useState(null);
+
   const [paidFor, setPaidFor] = useState(false);
   const [error, setError] = useState(null);
+
+  if (paidFor) {
+    alert("Thank you for your purchase!");
+  }
+
+  if (error) {
+    alert(error);
+  }
   const handleApprove = (orderID) => {
     setPaidFor(true);
   };
 
-  if (paidFor) {
-    console.log("Thank you for purchasing from Igrice Prodaja Balkan");
-  }
+  useEffect(() => {
+    if (cart.id) {
+      const generateToken = async () => {
+        try {
+          const token = await commerce.checkout.generateToken(cart.id, {
+            type: "cart",
+          });
 
-  if (error) {
-    console.log(error);
-  }
+          setToken(token);
+        } catch (err) {
+          console.log(err);
+        }
+      };
+
+      generateToken();
+    }
+  }, [cart]);
 
   return (
     <PayPalScriptProvider>
       <PayPalButtons
-        onClick={submit}
-        createOrder={(data, acitons) => {
-          return acitons.order.create({
+        onClick={(data, actions) => {
+          // Validate on button click, client or server side
+          const hasAlreadyBoughtCourse = false;
+
+          if (hasAlreadyBoughtCourse) {
+            setError(
+              "You already bought this course. Go to your account to view your list of courses."
+            );
+
+            return actions.reject();
+          } else {
+            return actions.resolve();
+          }
+        }}
+        createOrder={(data, actions) => {
+          return actions.order.create({
             purchase_units: [
               {
-                description: product.description,
                 amount: {
                   value: product.price,
                 },
@@ -32,18 +67,30 @@ export default function PaypalCheckoutButton({ product, submit }) {
             ],
           });
         }}
-        onApprove={async (data, aciton) => {
-          const order = await aciton.order.capture();
+        onApprove={async (data, actions) => {
+          const order = await commerce.checkout.capture(token.id, {
+            payment: {
+              gateway: "paypal",
+              paypal: {
+                action: "capture",
+                payment_id: "PAY-51028384J84281644LGFZXJQ",
+                payer_id: "VE57TQRTVER5Y",
+              },
+            },
+          });
           console.log("order", order);
 
           handleApprove(data.orderID);
         }}
-        onCancel={() => {}}
+        onCancel={() => {
+          // Display cancel message, modal or redirect user to cancel page or back to cart
+        }}
         onError={(err) => {
           setError(err);
-          console.log(err);
+          console.error("PayPal Checkout onError", err);
         }}
       />
+      ;
     </PayPalScriptProvider>
   );
 }
