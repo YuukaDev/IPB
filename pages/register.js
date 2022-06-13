c343173b317ff4ee1532489241dca31226894936
YuukaDev <vule.amv1@gmail.com>
2022-06-13 23:32:58 +0200

    Update register.js

--- pages/register.js
+++ pages/register.js
@@ -1,8 +1,15 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import useShop from "../utils/StoreContext";
+
 import Navigation from "../components/Navigation/Navigation"
 import Register from "../components/Authentication/Register"
 import Footer from "../components/Footer/Footer";
 
-export default function LoginPage() {
+export default function RegisterPage() {
+    const { customer } = useShop();
+    const router = useRouter();
+
     return (
         <>
             <Navigation />
