bfaee379fcd105db70f49bcf80fbec3e2308ad78
YuukaDev <vule.amv1@gmail.com>
2022-06-19 18:31:01 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["cdn.chec.io"],
+    loader: "akamai",
+    path: ''
   },
+  eslint: {
+    ignoreDuringBuilds: true
+  }
 }
 
 module.exports = nextConfig
